7408de3f05006b163ee174acdadb94b6a7a74af7
Diego Souza <eliaspirante@gmail.com>
2016-04-11 13:25:56 -0300

    Ajustes.

--- tips.php
+++ tips.php
@@ -1,4 +1,6 @@
 <?php
+	#mysqldump -hHOST -uUSER -pPASSWORD -Ke --hex-blob --single-transaction --dump-date --tables --where="1 order by 1 desc limit 10000" DATABASENAME > out.sql
+	
     $data[] = array(
         "title" => "Mysql format result",
         "description" => "Make output from mysql terminal to less.",
